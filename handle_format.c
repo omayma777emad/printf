d40bb1da8a2e50b70b06a874ddddc90356a0862d
omayma777emad <omimaemad777@gmail.com>
2023-12-05 05:48:47 -0800

    format file

---
+++ handle_format.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * handle_content - Prints an argument based on its type
+ * @data: Formatted string in which to print the arguments.
+ * @args: List of arguments to be printed.
+ * @index: index
+ * @content: Buffer array to handle print.
+ * Return: 1 or 2;
+*/
+
+int handle_content(const char *data,  int *index, va_list args, char content[])
+{	
+	int i, chars; 
+	data data_type[] =
+	{
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'i', print_int},
+		{'d', print_int},
+		{'\0', NULL}
+	};
+	for (i = 0; data_type[i].data!= '\0'; i++)
+		if (data[*index] == data_type[i].data)
+			return (data_type[i].f(args, content));
+	if (data_type[i].data == '\0')
+	{
+		if (data[*index] == '\0')
+			return (-1);
+}
+return (chars);
+}

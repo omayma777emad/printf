a4b18670f062a191d77ba6183a1b1356261960f8
Maxst1270 <alisuna225@gmail.com>
2023-12-05 05:14:17 -0800

    nums

---
+++ nums_d.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/**
+ * print_int - handles the d case in printf 
+ *
+ * @args:arguments
+ * @content:array for print handling
+*/
+void print_int(va_list args, int content[])
+{
+	int v = va_args(arg, int);
+
+	if (v / 10 != 0)
+	{
+		nums(v);
+	}
+
+	if (v < 0)
+	{
+		_puttchar('-');
+		v = -v;
+	}
+
+	_putchar(v % 10 + '0');
+
+}

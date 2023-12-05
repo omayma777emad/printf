5c96881bef1d89c9f7edf6c44da4035388fc03d8
omayma777emad <omimaemad777@gmail.com>
2023-12-05 06:21:41 -0800

    file

---
+++ handle_write_char.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * handle_write_char - Prints a string
+ * @c: char types.
+ * @content: array to handle print
+ * Return: Number of chars
+*/
+
+int handle_write_char(char c, char content[])
+{
+	return (write(1, &buffer[0], 1));
+}

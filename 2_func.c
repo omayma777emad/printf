5da4c2035cccfbd2009ece4c292e6d62105b9495
omayma777emad <omimaemad777@gmail.com>
2023-12-07 14:01:23 -0800

    aa

---
+++ 2_func.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * printf_int - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int print_int(va_list args, char content[])
+{
+	int n = va_arg(args, int);
+	int end = n % 10, digit, ex = 1;
+	int number, chars = 1;
+	UNUSED(content);
+
+	n = n / 10;
+	number = n;
+
+	if (end < 0)
+	{
+		_putchar('-');
+		number = -number;
+		n = -n;
+		end = -end;
+		chars++;
+	}
+	if (number > 0)
+	{
+		while (number / 10 != 0)
+		{
+			ex = ex * 10;
+			number = number / 10;
+		}
+		number = n;
+		while (ex > 0)
+		{
+			digit = number / ex;
+			_putchar(digit + '0');
+			number = number - (digit * ex);
+			ex = ex / 10;
+		chars++;
+		}
+	}
+	_putchar(end + '0');
+
+	return (chars);
+}

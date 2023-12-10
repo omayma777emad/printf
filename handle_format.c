85977ce8c891f5bce146b491f092dbf3967d0e57
omayma777emad <omimaemad777@gmail.com>
2023-12-10 00:05:00 -0800

    main.h file

--- handle_format.c
+++ handle_format.c
@@ -17,7 +17,7 @@ int handle_content(va_list args, int index, char content[])
 		{'i', print_int},
 		{'d', print_int},
 		{'r', print_rev},
-		{'R', print_rotstring(va_list},
+		{'R', print_rotstring},
 		{'\0', NULL}
 		};
 	for (i = 0; spc[i].spc != '\0'; i++)
